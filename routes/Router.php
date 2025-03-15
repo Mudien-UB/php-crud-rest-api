9bc4b73d1714f69d97a3990a02bf31e5926ce208
Mudien-UB <mudienub@gmail.com>
2025-03-15 21:18:23 +0700

    fixing http request method

--- routes/Router.php
+++ routes/Router.php
@@ -1,5 +1,6 @@
 <?php
 namespace Routes;
+
 require_once __DIR__ . '/../app/controllers/ProductController.php';
 
 use App\Controllers\ProductController;
@@ -13,21 +14,35 @@ class Router {
         $this->routes = [
             'POST' => [
                 '/api/products/add' => [$productController, 'addProduct'],
-                '/api/products/delete' => [$productController, 'deleteProduct'],
-                '/api/products/update' => [$productController, 'updateProduct']
             ],
             'GET' => [
                 '/api/products' => [$productController, 'getAllProducts']
+            ],
+            'PUT' => [
+                '/api/products/update' => [$productController, 'updateProduct']
+            ],
+            'DELETE' => [
+                '/api/products/delete' => [$productController, 'deleteProduct']
             ]
         ];
     }
 
     public function run() {
+        header('Content-Type: application/json');
+
         $requestMethod = $_SERVER['REQUEST_METHOD'];
         $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
         if (isset($this->routes[$requestMethod][$path])) {
-            call_user_func($this->routes[$requestMethod][$path]);
+            $handler = $this->routes[$requestMethod][$path];
+
+            // Menangani input body untuk metode selain GET
+            if (in_array($requestMethod, ['POST', 'PUT', 'DELETE'])) {
+                $inputData = json_decode(file_get_contents("php://input"), true);
+                call_user_func($handler, $inputData);
+            } else {
+                call_user_func($handler);
+            }
         } else {
             http_response_code(404);
             echo json_encode(['error' => 'Route not found']);
